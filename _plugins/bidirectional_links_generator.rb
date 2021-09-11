107dbc7e1e5a6cb1e40d9a913c22c4a72467b7c2
kvaishak <kvaishak47@gmail.com>
2021-09-11 16:27:17 +0530

    Added Changes upto Aug 24

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -24,28 +24,28 @@ class BidirectionalLinksGenerator < Jekyll::Generator
 
         # Replace double-bracketed links with label using note title
         # [[A note about cats|this is a link to the note about cats]]
-        current_note.content = current_note.content.gsub(
+        current_note.content.gsub!(
           /\[\[#{title_from_filename}\|(.+?)(?=\])\]\]/i,
           anchor_tag
         )
 
         # Replace double-bracketed links with label using note filename
         # [[cats|this is a link to the note about cats]]
-        current_note.content = current_note.content.gsub(
+        current_note.content.gsub!(
           /\[\[#{note_potentially_linked_to.data['title']}\|(.+?)(?=\])\]\]/i,
           anchor_tag
         )
 
         # Replace double-bracketed links using note title
         # [[a note about cats]]
-        current_note.content = current_note.content.gsub(
+        current_note.content.gsub!(
           /\[\[(#{note_potentially_linked_to.data['title']})\]\]/i,
           anchor_tag
         )
 
         # Replace double-bracketed links using note filename
         # [[cats]]
-        current_note.content = current_note.content.gsub(
+        current_note.content.gsub!(
           /\[\[(#{title_from_filename})\]\]/i,
           anchor_tag
         )
@@ -54,8 +54,8 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       # At this point, all remaining double-bracket-wrapped words are
       # pointing to non-existing pages, so let's turn them into disabled
       # links by greying them out and changing the cursor
-      current_note.content = current_note.content.gsub(
-        /\[\[(.*)\]\]/i, # match on the remaining double-bracket links
+      current_note.content.gsub!(
+        /\[\[([^\]]+)\]\]/i, # match on the remaining double-bracket links
         <<~HTML.chomp    # replace with this HTML (\\1 is what was inside the brackets)
           <span title='There is no note that matches this link.' class='invalid-link'>
             <span class='invalid-link-brackets'>[[</span>
